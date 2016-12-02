696eeb4298aff93fd6455c698334d49875343693
Tarzan9192 <joshwhtny@yahoo.com>
2016-12-02 05:10:17 -0800

    Response implementation nearly complete

--- makeup.pl
+++ makeup.pl
@@ -2,28 +2,76 @@
 :- dynamic prop/1.
 :- dynamic n/1.
 :- dynamic adj/1.
+:- dynamic thing/1.
+
+execute(X,R):- s(X,[],R).
 
 say(X):- s(X,[]).
 
 % When property is not given.
-s(X,Y):-
+s(X,Y,R):-
   det(X,Z),
-  ap(Z,Y).
+  ap(Z,Y,P),
+  thing(P),
+  R = ['I know'],!.
 
-s(X,Y):-
+% When user enters contradicting adjective
+s(X,Y,R):-
   det(X,Z),
-  propf(Z,A),
-  ap(A,Y).
+  ap(Z,Y,P),
+  P = [_,Noun,_],
+  thing([_,Noun,RealAdj]),
+  R = ["No, it's", RealAdj],
+  \+ thing(P),!.
 
-propf([P|T],R):-
+s(X,Y,R):-
+  det(X,Z),
+  ap(Z,Y,P),
+  assert(thing(P)),
+  R = ['Ok'].
+
+% When the property IS given
+s(X,Y,R):-
+  det(X,Z),
+  propf(Z,A,P),
+  ap(A,Y,P),
+  thing(P),
+  R = ['I know'],!.
+
+  % When user enters contradicting adjective
+s(X,Y,R):-
+  det(X,Z),
+  propf(Z,A,P),
+  ap(A,Y,P),
+  P = [_,Noun,_],
+  thing([_,Noun,RealAdj]),
+  R = ["No, it's", RealAdj],
+  \+ thing(P),!.
+
+s(X,Y,R):-
+  det(X,Z),
+  propf(Z,A,P),
+  ap(A,Y,P),
+  assert(thing(P)),
+  R = ['Ok'].
+
+% This predicate represents a property phrase,
+% which consists of a property followed by a
+% prepostiional phrase, i.e. "...color of the...".
+propf([P|T],R,[P,_,_]):-
   desc(T,R),
   assert(prop(P)).
 
-desc(A,B):-
-  prep(A,C),
-  det(C,B).
+% This predicate represents a 'describer',
+% or a prepostiional phrase, i.e. "...of the...".
+desc(X,Y):-
+  prep(X,Z),
+  det(Z,Y).
 
-ap([N|T],R):-
+% This predicate represents an assignment phrase
+% i.e. "is <adjective>". Used to represent an adjective
+% being assigned to a noun.
+ap([N|T],R,[_,N,Adj]):-
   asign(T,Adj),
   assert(n(N)),
   assert(adj(Adj)),
